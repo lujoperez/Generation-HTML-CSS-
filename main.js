10e5ec0dd594404df32519b19e3353aee9662e34
lujoperez <lujo.perez21@gmail.com>
2023-09-07 19:44:07 -0300

    Clase Js palindrono y factorial

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
-alert("hola");
-var intentos = 0;
-var a = prompt("Ingresa tu nombre");
-alert("Bienvenido " + a);
-var b = parseInt(prompt("Ingresa el numero del 1 al 100"));
-var num = Math.floor(Math.random()* (100 - 1 + 1) + 1); 
-while (b > num || b < num) {
-    intentos++;
-    if(b>num){alert("muy alto");
-   var b = parseInt(prompt("ingresa de nuevo "));
-} else if(b < num) {
-    alert("muy bajo");
-   var b = parseInt(prompt("ingresa de nuevo "));
-} 
+
+function calcularFactorial() {
+    const numberFactorial = document.getElementById("factorial");
+
+    const resultado = document.getElementById("respuesta1");
+    const num = parseInt(numberFactorial.value); 
+
+    if (isNaN(num) || num <0) {
+        resultado.textContent= "Ingresa un nummero valido 0 igual a 0"
+        return;
+     }
+
+    if (num === 0 ||num === 1  ) { 
+      resultado.textContent= "  El factorial de " + num + " es 1 "; 
+      return; 
+    }
+    let factorial = 1;
+    for (let i = 2; i <= num ; i++){
+        factorial *= i ;
+    }
+    resultado.textContent= "  El factorial de " + num + " es " + factorial; 
+
 }
-if (b == num){
-    alert("felicidades "+ a + "ganaste con "+intentos+" intentos " + ", el numero era "+ num );
+/* Funcion polindromo verficar  */
+function verificarPalindromo() {
+    const inputPalindromo = document.getElementById("inputpalindromo"); 
+    const respuesta = document.getElementById("respuesta2");
+    const texto = inputPalindromo.value.toLowerCase().replace(/[^a-z]/g, "");
+
+    if (texto === "") {
+        respuesta.textContent = "Por favor ingresa una palabra valida ";
+        return;
+    }
+    const reverse = texto.split("").reverse().join("");
+
+    if (texto === reverse){
+        respuesta.textContent = "Si, la palabra "+ inputPalindromo.value + ". Es un palindromo ";
+    } else {
+        respuesta.textContent = 
+        "no, la palabra " + inputPalindromo.value + ". No es un palindromo "
+    }
+
 }
 
 
